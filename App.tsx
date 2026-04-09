927b1df5c195f35a44d97f201d09f509b2180699
Gustavo Mourao Mena Barreto <102484583+GustavommBarreto@users.noreply.github.com>
2026-04-09 18:41:18 +0000

    feat: implementadas telas de Login (com Firebase) e CadastroOps

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { Home } from './Home';
+//import { Home } from './Home';
+import Login from './Login';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Home />
+      <Login />
+      
       <StatusBar style='auto' />
     </SafeAreaProvider>
   );
